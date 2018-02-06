0938076c571eaaae1b4e11cfa8945639092ff550
NickMcG-PC <D11123772@mydit.ie>
2018-02-06 19:33:10 +0000

    Initial MouseOver UI

---
+++ Assets/CanvasControl.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework.Constraints;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasControl : MonoBehaviour
+{
+
+	public GameObject MouseOverUi;
+    
+   	public GameObject HealthBar;
+	private float _maxBarSize = 525.0f;
+
+	public GameObject NameText;
+
+	public GameObject InfoText;
+    	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (MouseOverUi.activeSelf)
+		{
+			Ray ray;
+			RaycastHit hit;
+			//set up our ray from screen to scene
+			ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			
+			//if we hit
+			if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+			{	
+				SetMouseUI(hit.collider.gameObject);
+			}
+		}
+	}
+
+	public void MouseOverEnter()
+	{
+		MouseOverUi.SetActive(true);
+	}
+
+	public void MouseOverExit()
+	{
+		MouseOverUi.SetActive(false);
+	}
+
+	void SetMouseUI(GameObject highlightedGameObject)
+	{
+		CharacterInfo myChar = highlightedGameObject.GetComponent<CharacterInfo>();
+		
+		//healthBar
+		Vector3 newscale = HealthBar.transform.localScale;
+		newscale.x = _maxBarSize *(myChar.HealthPercent);
+		HealthBar.transform.localScale = newscale;
+
+		NameText.GetComponent<Text>().text = myChar.Name;
+
+		InfoText.GetComponent<Text>().text = myChar.Info;
+	}
+}
---
+++ Assets/CharacterInfo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions.Comparers;
+
+public class CharacterInfo : MonoBehaviour
+{
+
+	public string Name;
+
+	public string Info;
+	
+	public int Health;
+
+	public int MaxHealth;
+
+	public float HealthPercent;
+
+	public int Strength;
+
+	public int Dexterity;
+
+	public int Constitution;
+
+	public float Damage;
+	
+	public float MoveSpeed;
+
+	public float AttackSpeed;
+
+	public GameObject TheCanvas;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		TheCanvas = GameObject.FindGameObjectWithTag("Canvas");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		HealthPercent = (float)Health / MaxHealth;
+
+	}
+
+	private void OnMouseEnter()
+	{
+		TheCanvas.GetComponent<CanvasControl>().MouseOverEnter();
+	}
+
+	private void OnMouseExit()
+	{
+		TheCanvas.GetComponent<CanvasControl>().MouseOverExit();
+		
+	}
+}
