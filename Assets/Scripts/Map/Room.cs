d35891e73bb92b2a92c7e8654a4d5f1f17b40721
NickMcG-PC <D11123772@mydit.ie>
2018-02-11 17:03:55 +0000

    starting Generation

---
+++ Assets/Scripts/Map/Room.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room
+{
+	public Vector3 Gridpos;
+
+	public int Type;
+
+	public bool DoorN, DoorS, DoorE, DoorW;
+
+	public Room(Vector3 myGridpos, int myType)
+	{
+		
+	}
+}
